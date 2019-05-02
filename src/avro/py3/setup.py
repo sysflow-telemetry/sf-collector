06f5f0bbf5c7f10fc3df2dff36b487e95bbdc85a
Frederico Araujo <frederico.araujo@ibm.com>
2019-05-02 20:32:41 +0000

    chagend description

--- src/avro/py3/setup.py
+++ src/avro/py3/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name = 'SysFlow Processing API',
     version = '0.1',    
-    description = ('Runs workflows for deploying deceptions'),    
+    description = ('Install SysFlow python API and utilities'),    
     packages=['sysflow'],
     package_data={'sysflow': ['schema.avsc']},
     install_requires=['avro-python3==1.8.2', 'avro-gen==0.3.0'],
