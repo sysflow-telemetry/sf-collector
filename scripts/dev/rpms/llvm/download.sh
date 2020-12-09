e293a99537672a9b062e6adc569a833599539b10
Frederico Araujo <frederico.araujo@ibm.com>
2020-12-09 08:52:31 -0500

    adding rpms for clang 9 for dev build with centos 8 base
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

---
+++ scripts/dev/rpms/llvm/download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ wget https://rpmfind.net/linux/centos/8.2.2004/AppStream/x86_64/os/Packages/llvm-toolset-9.0.1-1.module_el8.2.0+309+0c7b6b03.x86_64.rpm
+ wget https://rpmfind.net/linux/centos/8.2.2004/AppStream/x86_64/os/Packages/llvm-9.0.1-5.module_el8.2.0+461+2e15bd5f.x86_64.rpm
+ wget https://rpmfind.net/linux/centos/8.2.2004/AppStream/x86_64/os/Packages/lld-9.0.1-1.module_el8.2.0+309+0c7b6b03.x86_64.rpm
+ wget https://rpmfind.net/linux/centos/8.2.2004/AppStream/x86_64/os/Packages/lldb-9.0.1-2.module_el8.2.0+309+0c7b6b03.x86_64.rpm
+ wget https://rpmfind.net/linux/centos/8.2.2004/AppStream/x86_64/os/Packages/clang-9.0.1-2.module_el8.2.0+309+0c7b6b03.x86_64.rpm
+ wget https://rpmfind.net/linux/centos/8.2.2004/AppStream/x86_64/os/Packages/clang-libs-9.0.1-2.module_el8.2.0+309+0c7b6b03.x86_64.rpm
+ wget https://rpmfind.net/linux/centos/8.2.2004/AppStream/x86_64/os/Packages/llvm-libs-9.0.1-5.module_el8.2.0+461+2e15bd5f.x86_64.rpm
+ wget https://rpmfind.net/linux/centos/8.2.2004/AppStream/x86_64/os/Packages/lld-libs-9.0.1-1.module_el8.2.0+309+0c7b6b03.x86_64.rpm
+ wget https://rpmfind.net/linux/centos/8.2.2004/BaseOS/x86_64/os/Packages/libedit-3.1-23.20170329cvs.el8.x86_64.rpm
+ wget https://rpmfind.net/linux/centos/8.2.2004/AppStream/x86_64/os/Packages/python3-lldb-9.0.1-2.module_el8.2.0+309+0c7b6b03.x86_64.rpm
--- scripts/installUBIDependencyWoSubManager.sh
+++ scripts/installUBIDependencyWoSubManager.sh
@@ -37,9 +37,6 @@ trap cleanup EXIT
 
 if [ "${MODE}" == "base" ] ; then
     # packages for base image
-   # dnf -y install --disableplugin=subscription-manager \
-   #     http://mirror.centos.org/centos/8/BaseOS/x86_64/os/Packages/centos-gpg-keys-8.2-2.2004.0.1.el8.noarch.rpm http://mirror.centos.org/centos/8/BaseOS/x86_64/os/Packages/centos-repos-8.2-2.2004.0.1.el8.x86_64.rpm
-    #dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
     dnf -y update && \
     dnf -y install dnf-plugins-core epel-release
     dnf config-manager --set-enabled powertools
@@ -73,17 +70,20 @@ if [ "${MODE}" == "base" ] ; then
         snappy-devel \
         jsoncpp-devel \
         glog-devel \
-        llvm-toolset \
+        dkms \
         bc
 
-    dnf -y install \
-        --disableexcludes=all \
-        --disableplugin=subscription-manager \
-        --disablerepo=ubi-8-appstream \
-        --disablerepo=ubi-8-baseos \
-        --disablerepo=ubi-8-codeready-builder \
-        --enablerepo=epel \
-        dkms
+    DIR=$(pwd) && cd /build/rpms/llvm && ./install.sh && cd $DIR
+    
+    #dnf -y install \
+    #    --disableexcludes=all \
+    #    --disableplugin=subscription-manager \
+    #    --disablerepo=ubi-8-appstream \
+    #    --disablerepo=ubi-8-baseos \
+    #    --disablerepo=ubi-8-codeready-builder \
+    #    --enablerepo=epel \
+    #    dkms
+
 
 elif [ "${MODE}" == "test-extra" ] ; then
     # additional packages for testing
