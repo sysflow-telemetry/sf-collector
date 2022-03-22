fa393ae71c3a07a180b85756d3bd1689e4f80cc1
Frederico Araujo <frederico.araujo@ibm.com>
2022-03-22 22:03:24 +0000

    feat(build): pre-package and install dkms if not already installed
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- scripts/cpack/prepackage.sh
+++ scripts/cpack/prepackage.sh
@@ -8,10 +8,14 @@ cd $DIR && rm -rf build && mkdir -p build
 cp -a $DIR/../../bin $DIR/build/bin
 mkdir -p $DIR/build/driver && cp -a $DIR/../../modules/bin/falco-driver-loader $DIR/build/driver/.
 mkdir -p $DIR/build/driver/src && cp -a $DRIVER_SRC_PATH $DIR/build/driver/src/$(basename $DRIVER_SRC_PATH)
+mkdir -p $DIR/build/modules/src && cp -a $DIR/../../modules/src/dkms  $DIR/build/modules/src/.
+mkdir -p $DIR/build/modules/bin && cp -a $(which make)  $DIR/build/modules/bin/make
 cp -a $DIR/../service/systemd $DIR/build/service
 cp $DIR/../service/bin/sysflow $DIR/build/bin/.
 cp -a $DIR/../service/conf $DIR/build/conf
 cp $DIR/../service/driver/start $DIR/build/driver/.
 cp $DIR/../service/driver/cleanup $DIR/build/driver/.
+cp $DIR/../../LICENSE.md $DIR/build/.
+cp $DIR/../../README.md $DIR/build/.
 cd $DIR
 
