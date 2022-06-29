538d50627890ef13f9459c84d5447a1c08a2d7bd
Frederico Araujo <frederico.araujo@ibm.com>
2022-06-29 02:32:53 +0000

    fix(build): fix libsysflow installation paths
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- scripts/cpack/prepackage-libs.sh
+++ scripts/cpack/prepackage-libs.sh
@@ -5,10 +5,10 @@ DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BUILD_DIR=build-libs
 
 cd $DIR && rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR
-mkdir -p $DIR/$BUILD_DIR/lib/falcosecurity && cp -a /usr/lib/x86_64-linux-gnu/falcosecurity $DIR/$BUILD_DIR/lib/falcosecurity
-mkdir -p $DIR/$BUILD_DIR/include/falcosecurity && cp -a /usr/include/falcosecurity $DIR/$BUILD_DIR/include/falcosecurity
-mkdir -p $DIR/$BUILD_DIR/lib/sysflow && cp -a /usr/local/sysflow/lib $DIR/$BUILD_DIR/lib/sysflow
-mkdir -p $DIR/$BUILD_DIR/include/sysflow && cp -a /usr/local/sysflow/include/sysflow $DIR/$BUILD_DIR/include/sysflow
+mkdir -p $DIR/$BUILD_DIR/lib && cp -a /usr/lib/x86_64-linux-gnu/falcosecurity $DIR/$BUILD_DIR/lib/.
+mkdir -p $DIR/$BUILD_DIR/include && cp -a /usr/include/falcosecurity $DIR/$BUILD_DIR/include/.
+mkdir -p $DIR/$BUILD_DIR/lib && cp -a /usr/local/sysflow/lib $DIR/$BUILD_DIR/lib/sysflow
+mkdir -p $DIR/$BUILD_DIR/include && cp -a /usr/local/sysflow/include/sysflow $DIR/$BUILD_DIR/include/.
 cp $DIR/../../LICENSE.md $DIR/$BUILD_DIR/.
 cp $DIR/../../README.md $DIR/$BUILD_DIR/.
 cd $DIR
