762a5175599f18fb3873eebf128124f3d3470eb9
Frederico Araujo <frederico.araujo@ibm.com>
2022-10-14 03:57:00 +0000

    refactor(modules): split falco libs from falco driver to enable independent libs/driver version combinations
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- examples/run.sh
+++ examples/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export DRIVER_NAME=falco
+export FALCO_BPF_PROBE=""
+export DRIVERS_REPO=https://download.falco.org/driver
 /usr/bin/falco-driver-loader bpf && $DIR/callback
