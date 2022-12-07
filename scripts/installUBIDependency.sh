cc5d41d5bc050470a55ae663abecb6638c7c8f0b
Frederico Araujo <frederico.araujo@ibm.com>
2022-12-07 14:02:52 +0000

    fix(ubi): fix UBI subscription manager login to disable check for container building (new feature in UBI)
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- scripts/installUBIDependency.sh
+++ scripts/installUBIDependency.sh
@@ -54,6 +54,8 @@ trap cleanup EXIT
         echo 'Assume build on RHEL machines or install packages only in UBI repositories.'
     else
         echo "Login RHEL..."
+        # See https://access.redhat.com/discussions/5889431?tour=8
+        sed -i 's/\(def in_container():\)/\1\n    return False/g' /usr/lib64/python*/*-packages/rhsm/config.py
         subscription-manager register --username "$REGISTER_USER" --password "$REGISTER_PASSWORD" --auto-attach
     fi
 )
